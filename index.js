4c7e64d2b8656f6bef230677d44dc7d14d3207f9
Yadveer <134469422+Yadveer1@users.noreply.github.com>
2024-10-02 00:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let main = document.querySelector(".main");
+let home = document.querySelector(".home");
+let arr = document.querySelector(".arr");
+let hover = document.querySelector(".hover");
+main.addEventListener("mouseenter", () => {
+    arr.style.right="20px";
+    hover.style.left="0px";
+});
+home.addEventListener("mouseenter", () => {
+    arr.style.right="-40px";
+    hover.style.left="-146px";
+});
+
